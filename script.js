8e6d1a547dc02729ab59973892a37cab61b529ba
Jordan Bichot <jordan.bichot97@gmail.com>
2023-02-16 12:35:33 -0500

    Small Js changes. -Remove redundant movileView variable. -Add overflywY style

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-const container = document.querySelector(".container");
+const myBody = document.querySelector("body");
 const menuButton = document.querySelector(".menu-button");
 const menuList = document.querySelector(".menu-list");
 const dimLayer = document.querySelector(".dim-layer");
@@ -7,34 +7,34 @@ const dimLayer = document.querySelector(".dim-layer");
 let isMenuOpen = false;
 
 menuButton.addEventListener("click", (e) => {
-  let movileView = screen.width < 376 ? true : false;
-  if (movileView) {
-    if (!isMenuOpen) {
-      menuList.style.visibility = "visible";
-      menuList.style.animation = "slideFromRight 0.5s ease-in";
+  if (!isMenuOpen) {
+    menuList.style.visibility = "visible";
+    menuList.style.animation = "slideFromRight 0.5s ease-in";
 
-      dimLayer.style.visibility = "visible";
+    dimLayer.style.visibility = "visible";
 
-      menuButton.style.backgroundImage =
-        "url(assets/images/icon-menu-close.svg)";
-      menuButton.style.width = "35px";
-      menuButton.style.height = "35px";
+    menuButton.style.backgroundImage = "url(assets/images/icon-menu-close.svg)";
+    menuButton.style.width = "35px";
+    menuButton.style.height = "35px";
 
-      isMenuOpen = true;
-    } else {
-      menuList.style.animation = "slideToRight 0.5s ease-in";
-      setTimeout(function () {
-        menuList.style.visibility = "hidden";
-      }, 490);
+    myBody.style.overflowY = "hidden";
 
-      dimLayer.style.visibility = "hidden";
+    isMenuOpen = true;
+  } else {
+    menuList.style.animation = "slideToRight 0.5s ease-in";
+    setTimeout(function () {
+      menuList.style.visibility = "hidden";
+    }, 490);
 
-      menuButton.style.backgroundImage = "url(assets/images/icon-menu.svg)";
+    dimLayer.style.visibility = "hidden";
 
-      menuButton.style.width = "40px";
-      menuButton.style.height = "17px";
+    menuButton.style.backgroundImage = "url(assets/images/icon-menu.svg)";
 
-      isMenuOpen = false;
-    }
+    menuButton.style.width = "40px";
+    menuButton.style.height = "17px";
+
+    myBody.style.overflowY = "visible";
+
+    isMenuOpen = false;
   }
 });
